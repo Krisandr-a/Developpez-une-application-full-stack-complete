c57cd5f0ddd16c438e0f885a201eaffe114585b0
krisandra <krisandra.adams@gmail.com>
2025-05-30 11:24:04 +0200

    feat: created basic users table via the model User (no security)

---
+++ back/src/main/java/com/openclassrooms/mddapi/model/User.java
@@ -0,0 +1,58 @@
+package com.openclassrooms.mddapi.model;
+
+import javax.persistence.*;
+import lombok.*;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "USERS")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+// add "implements UserDetails" and the methods below when implementing
+// security login
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
+    //methods from UserDetails interface for authorization
+
+    //returns the user’s roles list
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return List.of();
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return email;
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return password;
+//    }
+
+}
