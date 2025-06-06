e82b07e7d3c2a000fcd1067d42c7aaf31bae5ae8
krisandra <krisandra.adams@gmail.com>
2025-06-06 15:49:45 +0200

    feat: added buttons Create and Filter to Articles page

--- front/src/app/pages/articles/articles.component.ts
+++ front/src/app/pages/articles/articles.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./articles.component.scss']
 })
 export class ArticlesComponent implements OnInit {
+    isFilterOpen = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleFilter() {
+    this.isFilterOpen = !this.isFilterOpen;
   }
 
 }
