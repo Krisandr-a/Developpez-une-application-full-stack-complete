b4770d6ebdf2b6ddef40155fbcb9ae509c3c88e6
krisandra <krisandra.adams@gmail.com>
2025-07-11 10:03:44 +0200

    feat: only displays subbed theme articles

--- front/src/app/pages/articles/articles.component.ts
+++ front/src/app/pages/articles/articles.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticlesService, Article } from 'src/app/services/articles.service';
+import { SubscriptionService, UserThemeSubscriptionDto } from 'src/app/services/subscription.service';
 
 @Component({
   selector: 'app-articles',
@@ -12,13 +13,13 @@ export class ArticlesComponent implements OnInit {
   sortDescending = true;
 
 
-  constructor(private articlesService: ArticlesService) { }
+  constructor(
+    private articlesService: ArticlesService,
+    private subscriptionService: SubscriptionService
+  ) { }
 
   ngOnInit(): void {
-    this.articlesService.getAllArticles().subscribe({
-      next: (data) => this.articles = data,
-      error: (err) => console.error('Failed to fetch articles', err)
-    });
+    this.loadArticlesForSubscribedThemes();
   }
 
   toggleFilter() {
@@ -31,6 +32,24 @@ export class ArticlesComponent implements OnInit {
     );
 
     this.sortDescending = !this.sortDescending;
+  }
+
+  private loadArticlesForSubscribedThemes(): void {
+    this.subscriptionService.getUserSubscriptions().subscribe({
+      next: (subscriptions: UserThemeSubscriptionDto[]) => {
+        const themeIds = subscriptions.map(sub => sub.themeId);
+
+        this.articlesService.getAllArticles().subscribe({
+          next: (allArticles: Article[]) => {
+            this.articles = allArticles.filter(article =>
+              themeIds.includes(article.theme.id)
+            );
+          },
+          error: err => console.error('Failed to fetch articles', err)
+        });
+      },
+      error: err => console.error('Failed to fetch user subscriptions', err)
+    });
   }
 
 
