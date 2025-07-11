35e1b22807bac51c8072e8b332c89ab8e02a6107
krisandra <krisandra.adams@gmail.com>
2025-07-11 13:31:54 +0200

    feat: navbar is hidden on mobile devices on register and login pages

--- front/src/app/shared/navbar/navbar.component.ts
+++ front/src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { filter } from 'rxjs/operators';
@@ -11,10 +11,15 @@ import { filter } from 'rxjs/operators';
 export class NavbarComponent implements OnInit {
   hideLinks = false;
   hideNavbar = false;
+  isMobile = false;
+  // Pages where navbar should be hidden on mobile
+  mobilePages = ['/connexion', '/inscription'];
 
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.checkScreenSize();
+
     this.router.events
       .pipe(filter(event => event instanceof NavigationEnd))
       .subscribe(event => {
@@ -23,12 +28,32 @@ export class NavbarComponent implements OnInit {
 
         this.hideLinks = currentUrl.includes('/connexion') || currentUrl.includes('/inscription');
 
-        this.hideNavbar = currentUrl === '/';
+        // Always hides navbar on home page
+        if (currentUrl === '/') {
+          this.hideNavbar = true;
+          return;
+        }
 
+        // Hides navbar on mobile if on register or login page
+        if (this.isMobile && this.mobilePages.includes(currentUrl)) {
+          this.hideNavbar = true;
+          return;
+        }
+
+        // Displays navbar in all other situations
+        this.hideNavbar = false;
       });
 
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isMobile = window.innerWidth <= 600;
+  }
 
   logout() {
     this.authService.logout();
